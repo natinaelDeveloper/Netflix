6f438d6aff31ddc4c82657959baf3a9bc4ec8c22
Natiael Asfaw <natinael6570@gmail.com>
2025-10-15 03:50:26 -0700

    first commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>HELLO</p>
+    </>
+  )
+}
+
+export default App
