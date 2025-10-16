f3b5e5285743492c16c3723f86357413ef7e27cf
Natiael Asfaw <natinael6570@gmail.com>
2025-10-16 05:22:15 -0700

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
-import './App.css'
+import "./App.css"
+import Home from "./pages/Home/Home.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>HELLO</p>
+      <Home/>
     </>
   )
 }
---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from "../../components/Header/Header.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Home
+
