7d982dac8ab12542473d16af50dea4edb1b7a86c
Natiael Asfaw <natinael6570@gmail.com>
2025-10-19 05:26:21 -0700

    Banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import Header from "../../components/Header/Header.jsx";
+import Banner from "../../components/Banner/Banner.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
+
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
